18123124161f8bf58a31fb7bd15e319ca49cd8a2
root <you@example.com>
2022-12-06 07:35:42 -0800

    documented my module

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-
+"""
+idex for api
+"""
 from flask import jsonify, Flask
 from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
+    """return tuple"""
     return jsonify(status='OK')
