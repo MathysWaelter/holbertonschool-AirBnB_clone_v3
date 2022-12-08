8304e01ee72bb29d4d5eb5b729995e86eec02e12
root <you@example.com>
2022-12-08 10:21:39 -0800

    add CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,14 @@
 for start my api
 """
 from os import getenv
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, Blueprint
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
